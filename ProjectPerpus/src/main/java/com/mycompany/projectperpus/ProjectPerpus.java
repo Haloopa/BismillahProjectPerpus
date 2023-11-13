35b1a92f415df2a2545ed87fdd80da2d6239837b
Hanifah Lutfia Farah <111756211+Haloopa@users.noreply.github.com>
2023-11-13 20:11:46 +0700

    Projekbaruperpus

---
+++ ProjectPerpus/src/main/java/com/mycompany/projectperpus/ProjectPerpus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectperpus;
+
+/**
+ *
+ * @author Asus
+ */
+public class ProjectPerpus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
